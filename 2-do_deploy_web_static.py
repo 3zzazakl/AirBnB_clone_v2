1e6318fcabef58f827b79e3c0f48863388ee5db9
3zzazakl <vanzekovichgr@gmail.com>
2024-04-09 08:54:34 +0200

    fixing decompress

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ import os
 
 env.hosts = ['107.23.102.134', '18.210.17.11']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/deploy_airbnb.pub'
+env.key_filename = '~/.ssh/deploy_airbnb'
 
 def do_deploy(archive_path):
     "distributes an archive to your web servers, using the function"
@@ -16,24 +16,27 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/')
 
         # extract filename
-        name = archive_path.split('/')[-1].split('.')[0]
+        name = archive_path.split('/')[-1]
+        name_not_exist = name.split('.')[0]
 
         # create directory
-        path = '/data/web_static/releases/{}'.format(name)
-        run('mkdir {}'.format(path))
+        path = '/data/web_static/releases/{}'.format(name_not_exist)
+        run('mkdir -p {}'.format(path))
 
         # decompress archive
-        run('tar -xzf /tmp/{} -C {}'.format("/data/web_static/releases/test", path))
-        print("Successully Decompressed the archive YAYAAY")
+        run('tar -xzf /tmp/{} -C {}'.format(name, path))
+        print("Successfully Decompressed the archive YAYAAY")
 
         # delete archive
-        run('rm /tmp/{}'.format(archive_path.split('/')[-1]))
+        run('rm /tmp/{}'.format(name))
+        run('mv {}web_static/* {}'.format(path, path))
 
         # delete symbolic link
+        run('rm -rf {}web_static'.format(path))
         run('rm -rf /data/web_static/current')
 
         # create new symbolic link
-        run("ln -s {} /data/web_static/current").format()
+        run("ln -s {} /data/web_static/current".format(path))
 
         print("Deployment Successfully")
         return True

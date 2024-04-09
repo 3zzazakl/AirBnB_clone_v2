08d4f71928fa228fd3faebb79c11c22fec9fa5b0
3zzazakl <vanzekovichgr@gmail.com>
2024-04-09 07:37:52 +0200

    check deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,16 +18,12 @@ def do_deploy(archive_path):
         archive_name = archive_path.split('/')[-1]
         archive_name_noext = archive_name.split('.')[0]
         run('mkdir -p /data/web_static/releases/{}'.format(archive_name_noext))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.
-            format(archive_name, archive_name_noext))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(archive_name, archive_name_noext))
         run('rm /tmp/{}'.format(archive_name))
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/\
-            releases/{}/'.format(archive_name_noext, archive_name_noext))
-        run('rm -rf /data/web_static/releases/{}/web_static'.
-            format(archive_name_noext))
+        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(archive_name_noext, archive_name_noext))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(archive_name_noext))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{} /data/web_static/current'.
-            format(archive_name_noext))
+        run('ln -s /data/web_static/releases/{} /data/web_static/current'.format(archive_name_noext))
         return True
     except Exception as e:
         return False

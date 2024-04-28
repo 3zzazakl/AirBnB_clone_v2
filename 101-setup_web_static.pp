74f3d83f15e91a25895ef622700f5eb52508b646
3zzazakl <vanzekovichgr@gmail.com>
2024-04-28 22:02:52 +0300

    checking puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -8,79 +8,80 @@ $nginx_config = "server {
     index index.html index.htm;
     location /hbnb_static {
         alias /data/web_static/current;
-        autoindex on;
+        index index.html index.htm;
     }
     location /redirect_me {
         return 301 https://www.youtube.com;
     }
     error_page 404 /404.html;
-    location = /404 {
+    location /404 {
         root /var/www/html;
         internal;
     }
 }"
 
 package { 'nginx':
-    ensure => installed;
+    ensure   => 'installed',
+    provider => apt
 }
 
--> file { '/data/':
-    ensure => directory;
+-> file { '/data':
+    ensure => 'directory'
 }
 
 -> file { '/data/web_static/':
-    ensure => directory;
+    ensure => 'directory'
 }
 
 -> file { '/data/web_static/releases':
-    ensure => directory;
+    ensure => 'directory'
 }
 
 -> file { '/data/web_static/releases/test':
-    ensure => directory;
+    ensure => 'directory'
 }
 
 -> file { '/data/web_static/releases/test/index.html':
-    ensure => file;
-    content => "Holberton School\n";
+    ensure  => 'file',
+    content => "Holberton School\n"
 }
 
 -> file { '/data/web_static/shared':
-    ensure => directory;
+    ensure => 'directory'
 }
 
 -> file { '/data/web_static/current':
-    ensure => link;
-    target => '/data/web_static/releases/test';
+    ensure => 'link',
+    target => '/data/web_static/releases/test'
 }
 
 -> exec { 'chown_data':
-    command => 'chown -R ubuntu:ubuntu /data';
+    command => 'chown -R ubuntu:ubuntu /data'
 }
 
 -> file { '/var/www':
-    ensure => directory;
+    ensure => 'directory'
 }
 
 -> file { '/var/www/html':
-    ensure => directory;
+    ensure => 'directory'
 }
 
 -> file { '/var/www/html/index.html':
-    ensure => file;
-    content => "Holberton School\n";
+    ensure  => 'file',
+    content => "Holberton School\n"
 }
 
 -> file { '/var/www/html/404.html':
-    ensure => file;
-    content => "Ceci n'est pas la page\n";
+    ensure  => 'file',
+    content => "Ceci n'est pas la page\n"
 }
 
 -> file { '/etc/nginx/sites-available/default':
-    ensure => file;
-    content => $nginx_config;
+    ensure  => 'file',
+    content => $nginx_config
 }
 
 -> service { 'nginx':
-    ensure => running;
+    ensure => 'running'
 }

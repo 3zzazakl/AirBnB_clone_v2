a143ccff51826b486c5aed67f34fed245681e048
3zzazakl <vanzekovichgr@gmail.com>
2024-05-06 19:23:24 +0300

    fiz city

--- models/city.py
+++ models/city.py
@@ -15,10 +15,10 @@ class City(BaseModel, Base):
 
     __tablename__ = "cities"
     if storage_type == "db":
-        id = Column(String(60), primary_key=True, nullable=False)
+        id = Column(String(60), primary_key=True)
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        places = relationship('Place', cascade="all, delete", backref="cities")
+        places = relationship("Place", cascade="all, delete", backref="cities")
 
     else:
         state_id = ""
--- models/place.py
+++ models/place.py
@@ -11,7 +11,7 @@ storage_type = getenv("HBNB_TYPE_STORAGE")
 place_amenity = Table(
     'place_amenity',
     Base.metadata,
-    Column('place_id', String(60), ForeignKey('places.id'),
+    Column('place_id', String(60), ForeignKey("places.id"),
            primary_key=True, nullable=False),
     Column('amenity_id', String(60), ForeignKey('amenities.id'),
            primary_key=True, nullable=False)

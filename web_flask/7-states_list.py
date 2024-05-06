c5c767524622459b21c5542c915bccf5525f9321
3zzazakl <vanzekovichgr@gmail.com>
2024-05-06 18:02:05 +0300

    setting

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,14 +14,14 @@ def teardown_db(exception):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def states_list():
     """Display a HTML page: (inside the tag BODY)"""
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    return render_template("7-states_list.html", states=sorted_states)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """Listening on"""
     app.run(host="0.0.0.0", port=5000)

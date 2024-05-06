644c01806c71221a8ca785697b49d93768423377
3zzazakl <vanzekovichgr@gmail.com>
2024-05-06 19:29:19 +0300

    adding ciew

--- models/place.py
+++ models/place.py
@@ -9,11 +9,11 @@ from sqlalchemy.orm import relationship
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
 place_amenity = Table(
-    'place_amenity',
+    "place_amenity",
     Base.metadata,
-    Column('place_id', String(60), ForeignKey("places.id"),
+    Column("place_id", String(60), ForeignKey("places.id"),
            primary_key=True, nullable=False),
-    Column('amenity_id', String(60), ForeignKey('amenities.id'),
+    Column("amenity_id", String(60), ForeignKey("amenities.id"),
            primary_key=True, nullable=False)
 )
 
@@ -32,8 +32,12 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        amenities = relationship("Amenity", secondary="place_amenity",
-                                 viewonly=False)
+        reviews = relationship("Review", cascade="all, delete",
+                               backref="place")
+        amenities = relationship("Amenity",
+                                 secondary="place_amenity",
+                                 viewonly=False,
+                                 back_populates="place_amenities")
         amenity_ids = []
     else:
         city_id = ""

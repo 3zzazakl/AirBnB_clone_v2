d8d5dbd267ca962a9d2a199397d44d7d7afe8659
3zzazakl <vanzekovichgr@gmail.com>
2024-05-06 19:03:27 +0300

    setting city id

--- models/city.py
+++ models/city.py
@@ -15,6 +15,7 @@ class City(BaseModel, Base):
 
     __tablename__ = "cities"
     if storage_type == "db":
+        id = Column(String(60), primary_key=True, nullable=False)
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
         places = relationship('Place', cascade="all, delete", backref="cities")

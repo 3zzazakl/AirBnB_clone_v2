9f749b20a261e2d99c2f7efa917a1909e46de932
3zzazakl <vanzekovichgr@gmail.com>
2024-05-06 18:14:59 +0300

    cities

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,4 +8,22 @@ from models.city import City
 
 app = Flask(__name__)
 
-@
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page: (inside the tag BODY)"""
+    states = storage.all(State).values()
+    cities = storage.all(City).values()
+    return render_template("8-cities_by_states.html",
+                           states=states, cities=cities)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    """Listening on"""
+    app.run(host="0.0.0.0", port=5000)

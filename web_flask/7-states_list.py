835859765aa8441172ff968536ee6065d2111d0c
3zzazakl <vanzekovichgr@gmail.com>
2024-05-06 18:07:34 +0300

    checkcing task8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,18 +8,18 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """Remove the current SQLAlchemy Session"""
-    storage.close()
-
-
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Display a HTML page: (inside the tag BODY)"""
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)
     return render_template("7-states_list.html", states=sorted_states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
 
 
 if __name__ == "__main__":
